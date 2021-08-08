2a37e81d16ef39ceaf4126968c8454644f6be7ad
Piaotian Jin <piaoapiaotian@gmail.com>
2021-08-08 17:20:03 -0400

    setup and push code to repo

---
+++ task1/task1.py
@@ -0,0 +1,113 @@
+import dropbox
+import datetime
+import random
+import pandas as pd 
+import io
+
+# Connect to dropbox using access token
+TOKEN = ''
+dbx = dropbox.Dropbox(TOKEN)
+
+def main():
+    """Main program
+
+    Download csv from dropbox, then anonymize date of consent and add age, finally upload csv to dropbox
+    """
+    df = downloadData("/recruitment_project/enroll_data.csv")
+    offset_df = anonyDate(df)
+    addAge(df)
+    uploadData(offset_df, "/recruitment_project/enroll_data_offset_PJ.csv", dropbox.files.WriteMode.overwrite, False)
+    uploadData(df, "/recruitment_project/enroll_data_anon_PJ.csv", dropbox.files.WriteMode.overwrite, True)
+
+
+def downloadData(path):
+    """Download csv from dropbox 
+    
+    Download csv from dropbox, transform to a DataFrame, and return the DataFrame
+    """
+    try:
+        _metadata, res = dbx.files_download(path)
+    except dropbox.exceptions.HttpError as err:
+        print('*** HTTP error', err)
+        return None
+    
+    with io.BytesIO(res.content) as stream:
+        data = pd.read_csv(stream, index_col=0)
+    
+    print('download success')
+    return data
+
+
+def uploadData(df, path, mode, index):
+    """Upload csv to dropbox
+
+    Take a DataFrame, and upload the data to dropbox as csv
+    """
+    df_string = df.to_csv(index=index)
+    db_bytes = bytes(df_string, 'utf8')
+    
+    try:
+        res = dbx.files_upload(db_bytes, path, mode, mute=True)
+    except dropbox.exceptions.ApiError as err:
+        print('*** API error', err)
+        return None
+
+    print('uploaded as', res.name.encode('utf8'))
+    return res
+
+ 
+def anonyDate(df):
+    """Anonymize date of consent
+
+    Anonymize date of consent to a random date between first and last date of 1924, then calculate the offset between date of consent and random date.
+    Finally return offset DataFrame 
+    """
+    offset = []
+    for index, row in df.iterrows():
+        randomDate = generateRandomDate(datetime.date(1924, 1, 1), datetime.date(1924, 12, 31))
+        dateList = row["date of consent"].split("/")
+        date = datetime.date(int(dateList[2]), int(dateList[0]), int(dateList[1]))
+        offset.append((date - randomDate).days)
+        row["date of consent"] = randomDate
+    df.rename(columns = {"date of consent": "disguised date of consent"}, inplace = True)
+    offset_df = pd.DataFrame(offset)
+    offset_df.columns = ["days_offset"]
+    return offset_df
+
+
+def generateRandomDate(startDate, endDate):
+    """Generate a random date
+    
+    Calculate a random date between startDate and endDate, then return the generated random date
+    """
+    timeBetweenDates = endDate - startDate
+    daysBetweenDates = timeBetweenDates.days
+    randomDays = random.randrange(daysBetweenDates)
+    randomDate = startDate + datetime.timedelta(days=randomDays)
+    return randomDate
+
+
+def addAge(df):
+    """
+    Replace birth date column to age
+    """
+    for index, row in df.iterrows():
+        year = int(row["birth date"][0:4])
+        month = int(row["birth date"][5:7])
+        day = int(row["birth date"][8:])
+        row["birth date"] = calculateAge(datetime.datetime(year, month, day))
+    df.rename(columns = {"birth date": "age"}, inplace = True)
+
+
+def calculateAge(birthDate):
+    """Calculate age
+    
+    Calculate age based on birthDate, then return age
+    """
+    today = datetime.date.today()
+    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+    return age
+
+
+if __name__ == '__main__':
+    main()
---
+++ task2/task2.js
@@ -0,0 +1,168 @@
+let filteredData = data;
+let group = 'cohort';
+let sites;
+let today = new Date();
+const chartData = {};
+const startDateInput = document.querySelector('#startDate');
+const endDateInput = document.querySelector('#endDate');
+
+// Set end date input can't earlier than start date input
+startDateInput.addEventListener('change', (e) => {
+  endDateInput.min = e.target.value;
+});
+
+// Set start date input can't later than end date input
+endDateInput.addEventListener('change', (e) => {
+  startDateInput.max = e.target.value;
+});
+
+// Initialize data
+function initializeData() {
+  const siteSet = new Set();
+  // Create array of sites
+  // Change date of consent string to Date object
+  // Calculate age and add to data
+  data.forEach((record) => {
+    siteSet.add(record['site ID']);
+    record['date of consent'] = new Date(record['date of consent']);
+    record['age'] = calculateAge(record['birth date']);
+  });
+  sites = Array.from(siteSet);
+  sites.forEach((site) => {
+    chartData[site] = {};
+  });
+}
+
+// Calculate age based on birth date and today's date
+function calculateAge(dateString) {
+  const birthDate = new Date(dateString);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+// Handle group change
+function handleGroup(value) {
+  if (value === 'cohort') {
+    group = 'cohort';
+    handleCohortGroup();
+  } else if (value === 'age') {
+    group = 'age';
+    handleAgeGroup();
+  }
+  generateChart();
+}
+
+// Generate chartData when cohort is selected as group
+function handleCohortGroup() {
+  for (const site in chartData) {
+    chartData[site] = {
+      CHR: 0,
+      HC: 0,
+    };
+  }
+  filteredData.forEach((item) => {
+    if (item.cohort === 'CHR') {
+      chartData[item['site ID']]['CHR'] += 1;
+    } else if (item.cohort === 'HC') {
+      chartData[item['site ID']]['HC'] += 1;
+    }
+  });
+}
+
+// Generate chartData when age is selected as group
+function handleAgeGroup() {
+  for (const site in chartData) {
+    chartData[site] = {
+      '< 20': 0,
+      '20-29': 0,
+      '30-39': 0,
+      '40-49': 0,
+      '50-59': 0,
+      '60+': 0,
+    };
+  }
+  filteredData.forEach((item) => {
+    if (item.age < 20) {
+      chartData[item['site ID']]['< 20'] += 1;
+    } else if (item.age < 30) {
+      chartData[item['site ID']]['20-29'] += 1;
+    } else if (item.age < 40) {
+      chartData[item['site ID']]['30-39'] += 1;
+    } else if (item.age < 50) {
+      chartData[item['site ID']]['40-49'] += 1;
+    } else if (item.age < 60) {
+      chartData[item['site ID']]['50-59'] += 1;
+    } else {
+      chartData[item['site ID']]['60+'] += 1;
+    }
+  });
+}
+
+// Filter data based on date of consent by the time period from user input
+function handleFilter() {
+  const startDate = new Date(startDateInput.value.replace(/-/g, '/'));
+  const endDate = new Date(endDateInput.value.replace(/-/g, '/'));
+  filteredData = data.filter(
+    (item) =>
+      !(item['date of consent'] < startDate) &&
+      !(item['date of consent'] > endDate)
+  );
+  handleGroup(group);
+}
+
+// Format chartData so that can be used to draw chart
+function formatChartData() {
+  const groups = Object.keys(chartData[sites[0]]);
+  const seriesData = [];
+
+  groups.forEach((group) => {
+    const data = [];
+    sites.forEach((site) => {
+      data.push(chartData[site][group]);
+    });
+    seriesData.push({ name: group, data });
+  });
+  return seriesData;
+}
+
+// Generate barchart using Highcharts
+function generateChart() {
+  const data = formatChartData();
+  Highcharts.chart('chart', {
+    chart: {
+      type: 'column',
+    },
+    title: {
+      text: undefined,
+    },
+    xAxis: {
+      categories: sites,
+    },
+    yAxis: {
+      title: {
+        text: 'Enrollment',
+      },
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          formatter: function () {
+            return Math.round((100 * this.y) / this.total) + '%';
+          },
+        },
+      },
+    },
+    series: data,
+  });
+}
+
+// Run when the page first load
+initializeData();
+handleGroup('cohort');
+generateChart();
